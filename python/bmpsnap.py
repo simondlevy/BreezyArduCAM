3b911007c5ebbc0ae155b7bddcfa7be38460f201
simondlevy <simon.d.levy@gmail.com>
2017-08-01 14:34:59 -0400

    Added license info

--- python/bmpsnap.py
+++ python/bmpsnap.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
 '''
-save_bmp.py : save a single frame of BMP data from Arducam Mini
+bmpsnap.py : save a single frame of BMP data from Arducam Mini
 
-Adapted from https://github.com/dakoner/arducam_reader/blob/master/arducam_read.py
+Copyright (C) Simon D. Levy 2017
 
-License: Apache License 2.0
+This file is part of BreezyArduCAM.
+
+BreezyArduCAM is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+BreezyArduCAM is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with BreezyArduCAM.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
 import time
@@ -64,7 +75,7 @@ if __name__ == '__main__':
     # Get capture-done message
     getack(port)
     getack(port)
- 
+
     stdout.write('Writing file ...')
     stdout.flush()
 
--- python/jpgstream.py
+++ python/jpgstream.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
 '''
-stream_jpeg.py : display live ArduCAM JPEG images using OpenCV
+jpgstream.py : display live ArduCAM JPEG images using OpenCV
 
-Adapted from https://github.com/dakoner/arducam_reader/blob/master/arducam_read.py
+Copyright (C) Simon D. Levy 2017
 
-License: Apache License 2.0
+This file is part of BreezyArduCAM.
+
+BreezyArduCAM is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+BreezyArduCAM is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with BreezyArduCAM.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
 import time
