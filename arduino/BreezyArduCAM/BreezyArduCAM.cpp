78d6dd20bdebb112bc4ee283144bb1a4fbdef6e3
simondlevy <simon.d.levy@gmail.com>
2017-08-01 20:49:27 -0400

    Moved #define's into cpp

--- arduino/BreezyArduCAM/BreezyArduCAM.cpp
+++ arduino/BreezyArduCAM/BreezyArduCAM.cpp
@@ -27,6 +27,123 @@ along with BreezyArduCAM.  If not, see <http://www.gnu.org/licenses/>.
 #define Wire Wire1
 #endif
 
+#if defined (__AVR__)
+#define cbi(reg, bitmask) *reg &= ~bitmask
+#define sbi(reg, bitmask) *reg |= bitmask
+#define pulse_high(reg, bitmask) sbi(reg, bitmask); cbi(reg, bitmask);
+#define pulse_low(reg, bitmask) cbi(reg, bitmask); sbi(reg, bitmask);
+#define cport(port, data) port &= data
+#define sport(port, data) port |= data
+#define swap(type, i, j) {type t = i; i = j; j = t;}
+#define fontbyte(x) pgm_read_byte(&cfont.font[x])  
+#define regtype volatile uint8_t
+#define regsize uint8_t
+#endif
+
+#if defined(__SAM3X8E__)
+
+#define cbi(reg, bitmask) *reg &= ~bitmask
+#define sbi(reg, bitmask) *reg |= bitmask
+
+#define pulse_high(reg, bitmask) sbi(reg, bitmask); cbi(reg, bitmask);
+#define pulse_low(reg, bitmask) cbi(reg, bitmask); sbi(reg, bitmask);
+
+#define cport(port, data) port &= data
+#define sport(port, data) port |= data
+
+#define swap(type, i, j) {type t = i; i = j; j = t;}
+#define fontbyte(x) cfont.font[x]  
+
+#define regtype volatile uint32_t
+#define regsize uint32_t
+
+#define PROGMEM
+
+#endif
+
+#if defined(__CPU_ARC__)
+#define cbi(reg, bitmask) *reg &= ~bitmask
+#define sbi(reg, bitmask) *reg |= bitmask
+#define pulse_high(reg, bitmask) sbi(reg, bitmask); cbi(reg, bitmask);
+#define pulse_low(reg, bitmask) cbi(reg, bitmask); sbi(reg, bitmask);
+#define cport(port, data) port &= data
+#define sport(port, data) port |= data
+#define swap(type, i, j) {type t = i; i = j; j = t;}
+#define fontbyte(x) pgm_read_byte(&cfont.font[x])  
+#define regtype volatile uint32_t
+#define regsize uint32_t
+#endif
+
+
+/****************************************************/
+/* I2C Control Definition 						    */
+/****************************************************/
+
+//Define maximum frame buffer size
+#define MAX_FIFO_SIZE		0x5FFFF			//384KByte
+
+/****************************************************/
+/* ArduChip registers definition 											*/
+/****************************************************/
+#define RWBIT				    0x80  //READ AND WRITE BIT IS BIT[7]
+
+#define ARDUCHIP_TEST1       	0x00  //TEST register
+
+#if !(defined OV2640_MINI_2MP)
+#define ARDUCHIP_FRAMES		    0x01  //FRAME control register, Bit[2:0] = Number of frames to be captured																		                  //On 5MP_Plus platforms bit[2:0] = 7 means continuous capture until frame buffer is full
+#endif
+
+#define ARDUCHIP_MODE      		0x02  //Mode register
+#define MCU2LCD_MODE       		0x00
+#define CAM2LCD_MODE       		0x01
+#define LCD2MCU_MODE       		0x02
+
+#define ARDUCHIP_TIM       		0x03  //Timming control
+#if !(defined OV2640_MINI_2MP)
+#define HREF_LEVEL_MASK    		0x01  //0 = High active , 		1 = Low active
+#define VSYNC_LEVEL_MASK   		0x02  //0 = High active , 		1 = Low active
+#define LCD_BKEN_MASK      		0x04  //0 = Enable, 					1 = Disable
+#if (defined ARDUCAM_SHIELD_V2)
+#define PCLK_REVERSE_MASK  	    0x08  //0 = Normal PCLK, 		1 = REVERSED PCLK
+#else
+#define PCLK_DELAY_MASK  		0x08  //0 = data no delay,		1 = data delayed one PCLK
+#endif
+#endif
+
+#define ARDUCHIP_FIFO      		0x04  //FIFO and I2C control
+#define FIFO_CLEAR_MASK    		0x01
+#define FIFO_START_MASK    		0x02
+#define FIFO_RDPTR_RST_MASK     0x10
+#define FIFO_WRPTR_RST_MASK     0x20
+
+#define ARDUCHIP_GPIO			  0x06  //GPIO Write Register
+#if !(defined (ARDUCAM_SHIELD_V2) || defined (ARDUCAM_SHIELD_REVC))
+#define GPIO_RESET_MASK			0x01  //0 = Sensor reset,							1 =  Sensor normal operation
+#define GPIO_PWDN_MASK			0x02  //0 = Sensor normal operation, 	1 = Sensor standby
+#define GPIO_PWREN_MASK			0x04	//0 = Sensor LDO disable, 			1 = sensor LDO enable
+#endif
+
+#define BURST_FIFO_READ			0x3C  //Burst FIFO read operation
+#define SINGLE_FIFO_READ		0x3D  //Single FIFO read operation
+
+#define ARDUCHIP_REV       		0x40  //ArduCHIP revision
+#define VER_LOW_MASK       		0x3F
+#define VER_HIGH_MASK      		0xC0
+
+#define ARDUCHIP_TRIG      		0x41  //Trigger source
+#define VSYNC_MASK         		0x01
+#define SHUTTER_MASK       		0x02
+#define CAP_DONE_MASK      		0x08
+
+#define FIFO_SIZE1				0x42  //Camera write FIFO size[7:0] for burst to read
+#define FIFO_SIZE2				0x43  //Camera write FIFO size[15:8]
+#define FIFO_SIZE3				0x44  //Camera write FIFO size[18:16]
+
+
+/****************************************************/
+
+
+
 enum {
 
     OV2640_160x120, 		
