5f9d2ac220aad70261bf4a52b9f94e79bb43d19c
simondlevy <simon.d.levy@gmail.com>
2017-08-03 14:52:36 -0400

    =cleanup

--- arduino/BreezyArduCAM/BreezyArduCAM.cpp
+++ arduino/BreezyArduCAM/BreezyArduCAM.cpp
@@ -80,60 +80,25 @@ along with BreezyArduCAM.  If not, see <http://www.gnu.org/licenses/>.
 /****************************************************/
 /* ArduChip registers definition    	            */
 /****************************************************/
-#define RWBIT				    0x80  //READ AND WRITE BIT IS BIT[7]
 
-#define ARDUCHIP_TEST1       	0x00  //TEST register
+#define ARDUCHIP_TEST1       	0x00    //TEST register
 
-#if !(defined OV2640_MINI_2MP)
-#define ARDUCHIP_FRAMES		    0x01  //FRAME control register, Bit[2:0] = Number of frames to be captured																		                  //On 5MP_Plus platforms bit[2:0] = 7 means continuous capture until frame buffer is full
-#endif
-
-#define ARDUCHIP_MODE      		0x02  //Mode register
-#define MCU2LCD_MODE       		0x00
-#define CAM2LCD_MODE       		0x01
-#define LCD2MCU_MODE       		0x02
-
-#define ARDUCHIP_TIM       		0x03  //Timming control
-#if !(defined OV2640_MINI_2MP)
-#define HREF_LEVEL_MASK    		0x01  //0 = High active , 		1 = Low active
-#define VSYNC_LEVEL_MASK   		0x02  //0 = High active , 		1 = Low active
-#define LCD_BKEN_MASK      		0x04  //0 = Enable, 					1 = Disable
-#if (defined ARDUCAM_SHIELD_V2)
-#define PCLK_REVERSE_MASK  	    0x08  //0 = Normal PCLK, 		1 = REVERSED PCLK
-#else
-#define PCLK_DELAY_MASK  		0x08  //0 = data no delay,		1 = data delayed one PCLK
-#endif
-#endif
-
-#define ARDUCHIP_FIFO      		0x04  //FIFO and I2C control
+#define ARDUCHIP_FIFO      		0x04    //FIFO and I2C control
 #define FIFO_CLEAR_MASK    		0x01
 #define FIFO_START_MASK    		0x02
-#define FIFO_RDPTR_RST_MASK     0x10
-#define FIFO_WRPTR_RST_MASK     0x20
 
-#define ARDUCHIP_GPIO			0x06  //GPIO Write Register
-#define GPIO_RESET_MASK			0x01  //0 = Sensor reset,				1 = Sensor normal operation
-#define GPIO_PWDN_MASK			0x02  //0 = Sensor normal operation, 	1 = Sensor standby
-#define GPIO_PWREN_MASK			0x04  //0 = Sensor LDO disable, 		1 = sensor LDO enable
-
-#define ARDUCHIP_TRIG      		0x41  //Trigger source
-#define VSYNC_MASK         		0x01
-#define SHUTTER_MASK       		0x02
+#define ARDUCHIP_TRIG      		0x41    //Trigger source
 #define CAP_DONE_MASK      		0x08
 
-#define BURST_FIFO_READ			0x3C  //Burst FIFO read operation
-#define SINGLE_FIFO_READ		0x3D  //Single FIFO read operation
+#define BURST_FIFO_READ			0x3C    //Burst FIFO read operation
+#define SINGLE_FIFO_READ		0x3D    //Single FIFO read operation
 
-#define ARDUCHIP_REV       		0x40  //ArduCHIP revision
-#define VER_LOW_MASK       		0x3F
-#define VER_HIGH_MASK      		0xC0
-
-#define FIFO_SIZE1				0x42  //Camera write FIFO size[7:0] for burst to read
-#define FIFO_SIZE2				0x43  //Camera write FIFO size[15:8]
-#define FIFO_SIZE3				0x44  //Camera write FIFO size[18:16]
+#define FIFO_SIZE1				0x42    //Camera write FIFO size[7:0] for burst to read
+#define FIFO_SIZE2				0x43    //Camera write FIFO size[15:8]
+#define FIFO_SIZE3				0x44    //Camera write FIFO size[18:16]
 
 //Define maximum frame buffer size
-#define MAX_FIFO_SIZE		    0x5FFFF			//384KByte
+#define MAX_FIFO_SIZE		    0x5FFFF //384KByte
 
 /****************************************************/
 
