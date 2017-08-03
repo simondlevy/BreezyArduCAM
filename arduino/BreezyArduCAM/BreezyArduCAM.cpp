d1e295f4da44230cdf8e9af189cde0968eb3b10d
simondlevy <simon.d.levy@gmail.com>
2017-08-03 14:57:17 -0400

    =cleanup

--- arduino/BreezyArduCAM/BreezyArduCAM.cpp
+++ arduino/BreezyArduCAM/BreezyArduCAM.cpp
@@ -63,19 +63,6 @@ along with BreezyArduCAM.  If not, see <http://www.gnu.org/licenses/>.
 
 #endif
 
-#if defined(__CPU_ARC__)
-#define cbi(reg, bitmask) *reg &= ~bitmask
-#define sbi(reg, bitmask) *reg |= bitmask
-#define pulse_high(reg, bitmask) sbi(reg, bitmask); cbi(reg, bitmask);
-#define pulse_low(reg, bitmask) cbi(reg, bitmask); sbi(reg, bitmask);
-#define cport(port, data) port &= data
-#define sport(port, data) port |= data
-#define swap(type, i, j) {type t = i; i = j; j = t;}
-#define fontbyte(x) pgm_read_byte(&cfont.font[x])  
-#define regtype volatile uint32_t
-#define regsize uint32_t
-#endif
-
 
 /****************************************************/
 /* ArduChip registers definition    	            */
