19f352c9db2e58f72ff4f85ced58938409ba78f0
simondlevy <simon.d.levy@gmail.com>
2017-08-06 18:24:32 -0400

    Begin grayscale streaming example

---
+++ arduino/BreezyArduCAM/examples/GrayStream/GrayStream.ino
@@ -0,0 +1,46 @@
+/*
+GrayStream.ino : stream grayscale image bytes to host
+
+Copyright (C) Simon D. Levy 2017
+
+This file is part of BreezyArduCAM.
+
+BreezyArduCAM is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+BreezyArduCAM is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with BreezyArduCAM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <Wire.h>
+#include <BreezyArduCAM.h>
+#include <SPI.h>
+
+// set pin 10 as the slave select for the digital pot:
+static const int CS = 10;
+
+Serial_ArduCAM_Mini_2MP myCam(CS);
+
+void setup(void) 
+{
+    // ArduCAM Mini uses both I^2C and SPI buses
+    Wire.begin();
+    SPI.begin();
+
+    // Talk to Arduino at fastest possible baud rate
+    Serial.begin(921600);
+
+    // Begin capturing in  QVGA mode, scale-down factor 2^1 = 2
+    myCam.beginQvga(2);
+}
+
+void loop(void) 
+{
+    myCam.capture();
+}
---
+++ python/graystream.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+'''
+graystream.py : Use OpenCV to display streaming grayscale images from ArduCAM Mini
+
+Copyright (C) Simon D. Levy 2017
+
+This file is part of BreezyArduCAM.
+
+BreezyArduCAM is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+BreezyArduCAM is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with BreezyArduCAM.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+import time
+import serial
+from sys import stdout
+
+from helpers import *
+
+# Modifiable params -------------------------------------------------------------------
+
+PORT = '/dev/ttyACM0' # Ubuntu
+#PORT = 'COM4'         # Windows
+
+SCALEDOWN = 2          # logarithm of 2 (e.g., SCALEDOWN=3 gives 1/8 width, 1/8 height)
+
+BAUD = 921600   # Arduino Uno
+
+OUTFILENAME = 'test.bmp'
+
+# helpers  --------------------------------------------------------------------------
+
+def num2bytes(n):
+    return [n&0xFF, (n>>8)&0xFF, (n>>16)&0xFF, (n>>24)&0XFF]
+
+def dump(msg):
+    stdout.write(msg)
+    stdout.flush()
+
+# BMP header for 320x240 imagel ------------------------------------------------------
+#
+# See: http://www.fastgraph.com/help/bmp_header_format.html
+# See: https://upload.wikimedia.org/wikipedia/commons/c/c4/BMPfileFormat.png
+
+header = [
+    0x42, 0x4D,             # signature, must be 4D42 hex
+    0x36, 0x58, 0x02, 0x00, # size of BMP file in bytes (unreliable)
+    0x00, 0x00,             # reserved, must be zero
+    0x00, 0x00,             # reserved, must be zero
+    0x42, 0x00, 0x00, 0x00, # offset to start of image data in bytes
+    0x28, 0x00, 0x00, 0x00, # size of BITMAPINFOHEADER structure, must be 40
+    0x40, 0x01, 0x00, 0x00, # image width in pixels
+    0xF0, 0x00, 0x00, 0x00, # image height in pixels
+    0x01, 0x00,             # number of planes in the image, must be 1
+    0x10, 0x00,             # number of bits per pixel
+    0x03, 0x00, 0x00, 0x00, # compression type
+    0x00, 0x58, 0x02, 0x00, # size of image data in bytes (including padding)
+    0xC4, 0x0E, 0x00, 0x00, # horizontal resolution in pixels per meter (unreliable)
+    0xC4, 0x0E, 0x00, 0x00, # vertical resolution in pixels per meter (unreliable)
+    0x00, 0x00, 0x00, 0x00, # number of colors in image, or zero
+    0x00, 0x00, 0x00, 0x00, # number of important colors, or zero
+    0x00, 0xF8, 0x00, 0x00, # red channel bitmask
+    0xE0, 0x07, 0x00, 0x00, # green channel bitmask
+    0xE0, 0x07, 0x00, 0x00  # blue channel bitmask
+]
+
+# header modification for different image sizes -------------------------------------
+
+width  = 320 >> SCALEDOWN
+height = 240 >> SCALEDOWN
+
+header[18:22] = num2bytes(width)
+header[22:26] = num2bytes(height)
+header[34:38] = num2bytes(width*height*2)
+
+# main ------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+
+    # Open connection to Arduino with a timeout of two seconds
+    port = serial.Serial(PORT, BAUD, timeout=2)
+
+    dump('Starting capture ...')
+
+    # Validate startup message
+    ackcheck(port, 'SPI interface OK.')
+
+    # Wait a spell
+    time.sleep(0.2)
+
+    # Send "start capture" message
+    sendbyte(port, 1)
+
+    dump('\nWriting file %s (will appear upside-down) ...' % OUTFILENAME)
+
+    # Open output file
+    outfile = open(OUTFILENAME, 'wb')
+
+    # Write BMP header
+    outfile.write(bytearray(header))
+
+    # Read bytes from serial and write them to file
+    for k in range((320>>SCALEDOWN)*(240>>SCALEDOWN)*2):
+        c = outfile.write(port.read())
+
+    # Send "stop" message
+    sendbyte(port, 0)
+
+    # Close output file
+    outfile.close()
+
+    print('\nDone')
+
